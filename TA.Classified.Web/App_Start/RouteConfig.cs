44a75f9392c432a99805d2c8bbc55c5609b6214c
durgaprasadr <durgaprasadr@techaspect.com>
2016-04-25 21:24:22 +0530

    test

--- TA.Classified.Web/App_Start/RouteConfig.cs
+++ TA.Classified.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace TA.Classified.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //Test
         }
     }
 }
